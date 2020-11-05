6b4204d4b055c548e047759ad92e6baf875be265
Severi Vidnäs <severi.vidnas@gmail.com>
2020-11-05 20:56:58 +0200

    Change earth to area2d, destroy anything on impact

--- earth/earth.gd
+++ earth/earth.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Area2D
 
 # Signal when earth gets hit
 signal earth_hit
@@ -15,3 +15,9 @@ func _physics_process(delta):
 		rot -= 1
 	# Apply rotation
 	$MoonSpatial.rotation += PI * rot * delta
+
+
+func _on_Earth_body_entered(body):
+	emit_signal("earth_hit")
+	body.queue_free()
+	pass # Replace with function body.
