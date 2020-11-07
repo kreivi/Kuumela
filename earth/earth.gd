fc07dbb8526047921b1143888030991504c4c7f4
Severi Vidnäs <severi.vidnas@gmail.com>
2020-11-07 20:19:36 +0200

    Lerp speed to make it feel less snappy, scale gfx down

--- earth/earth.gd
+++ earth/earth.gd
@@ -3,19 +3,29 @@ extends Area2D
 # Signal when earth gets hit
 signal earth_hit
 
+export (int) var speed = 3
+export (float, 0, 1.0) var deacceleration = 0.25
+export (float, 0, 1.0) var acceleration = 0.5
+
 # Export the speed so it can be altered via properties
 export var rotation_speed = 10
 
-func _physics_process(delta):
-	# Read movement input
-	var rot = 0
-	if Input.is_action_pressed("clockwise"):
-		rot += 1
-	if Input.is_action_pressed("counter_clockwise"):
-		rot -= 1
-	# Apply rotation
-	$MoonSpatial.rotation += PI * rot * delta
+var velocity = Vector2.ZERO
 
+func get_input():
+	var dir = 0;
+	if Input.is_action_pressed("clockwise"):
+		dir += 1
+	if Input.is_action_pressed("counter_clockwise"):
+		dir -= 1
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration);
+	else:
+		velocity.x = lerp(velocity.x, 0, deacceleration)
+
+func _physics_process(delta):
+	get_input()
+	$MoonSpatial.rotate(velocity.x * delta)
 
 func _on_Earth_body_entered(body):
 	emit_signal("earth_hit")
