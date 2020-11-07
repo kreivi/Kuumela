8029b31e801d72315fc614f0845fdf95eff8000b
Severi Vidnäs <severi.vidnas@gmail.com>
2020-11-07 20:21:39 +0200

    Score tracking

--- main/main.gd
+++ main/main.gd
@@ -1,11 +1,22 @@
 extends Node
 
+var score = 0
+
+func _ready():
+	new_game()
+	
 func new_game():
-	pass
+	score = 0
+	$ScoreTimer.start()
 
 func game_over():
-	pass
+	$ScoreTimer.stop()
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$GUI.set_score(score)
