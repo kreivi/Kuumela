890d5a64491f5b7e32c0783882e7e87dea846a9f
Severi Vidnäs <severi.vidnas@gmail.com>
2020-11-09 22:10:34 +0200

    Update asteroid gfx

--- asteroid/asteroid.gd
+++ asteroid/asteroid.gd
@@ -1,15 +1,8 @@
 extends RigidBody2D
 
-export var min_speed = 50
+export var min_speed = 75
 export var max_speed = 125
 
 func _on_screen_exited():
 	# Destroy as soon as screen is exited (if not destroyed already)
 	queue_free()
-
-
-func _on_Asteroid_body_entered(body):
-	if (body.is_in_group("asteroids")):
-		# Destroy when colliding with another asteroid
-		queue_free()
-	pass # Replace with function body.
