f3fa33f23c301f222ea50aa5769bb3c2a73af9c1
Severi Vidnäs <severi.vidnas@gmail.com>
2020-11-14 22:45:50 +0200

    GUI shows life and can reset status on new game

--- gui/gui.gd
+++ gui/gui.gd
@@ -1,4 +1,37 @@
 extends Control
 
+signal request_new
+
+func new_game():
+	$MainMenu.hide()
+	$Game.show()
+	set_lives(3)
+
+func game_over():
+	$Game.hide()
+	$MainMenu.show()
+	set_lives(0)
+	set_score(0)
+	
 func set_score(newscore):
-	$ScoreContainer/Score.text = str(newscore)
+	$Game/BottomPanel/ScoreContainer/Score.text = str(newscore)
+
+func set_life_visibilities(one, two, three):
+	$Game/BottomPanel/LivesContainer/Lives/Life1.visible = one
+	$Game/BottomPanel/LivesContainer/Lives/Life2.visible = two
+	$Game/BottomPanel/LivesContainer/Lives/Life3.visible = three
+		
+func set_lives(newLives):
+	# TODO: Ugly code blaargh í ¾í´®í ¾
+	if newLives == 3:
+		set_life_visibilities(true, true, true)
+	elif newLives == 2:
+		set_life_visibilities(false, true, true)
+	elif newLives == 1:
+		set_life_visibilities(false, false, true)
+	else:
+		set_life_visibilities(false, false, false)
+
+
+func _on_NewGame_pressed():
+	emit_signal('request_new')
