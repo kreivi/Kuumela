fd00027fd40a20b48cb26b77fc4ba859346b888b
Severi Vidnäs <severi.vidnas@gmail.com>
2020-11-14 22:45:06 +0200

    Restarting game is now possible

--- main/main.gd
+++ main/main.gd
@@ -1,15 +1,21 @@
 extends Node
 
 var score = 0
+var lives = 3
 
 func _ready():
 	new_game()
 	
 func new_game():
+	lives = 3
 	score = 0
+	$World.new_game()
+	$GUI.new_game()
 	$ScoreTimer.start()
 
 func game_over():
+	$World.game_over()
+	$GUI.game_over()
 	$ScoreTimer.stop()
 
 func _input(event):
@@ -20,3 +26,16 @@ func _input(event):
 func _on_ScoreTimer_timeout():
 	score += 1
 	$GUI.set_score(score)
+
+
+func _on_World_deduct_life():
+	lives -= 1
+	if lives > 0:
+		$GUI.set_lives(lives)
+	if lives <= 0:
+		game_over()
+
+
+
+func _on_GUI_request_new():
+	new_game()
