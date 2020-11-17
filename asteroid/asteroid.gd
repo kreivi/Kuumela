e2291f3015b70d64ab7b88326ae2ba3411ce2361
Severi Vidnäs <severi.vidnas@gmail.com>
2020-11-17 18:38:25 +0200

    Call impact_fx on body collision

--- asteroid/asteroid.gd
+++ asteroid/asteroid.gd
@@ -6,3 +6,10 @@ export var max_speed = 125
 func _on_screen_exited():
 	# Destroy as soon as screen is exited (if not destroyed already)
 	queue_free()
+
+
+func _on_Asteroid_body_entered(body):
+	$ImpactEffect.play()
+	if body.has_method("impact_fx"):
+		body.impact_fx(self)
+
